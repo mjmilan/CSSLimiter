d1dc8a628a05f9e9caf71852f91b5a1cbeb56c8c
Martin Milan <martin@mjmilan.com>
2014-02-27 00:24:29 +0000

    Initial Commit just to get something on GitHub

---
+++ CSSLimiter/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Martin
+ * Date: 27/02/2014
+ * Time: 00:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace CSSLimiter
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
