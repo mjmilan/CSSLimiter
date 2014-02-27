d3f897d36c42f22aea9b017eb52036b5f3f04a0b
Martin Milan <martin@mjmilan.com>
2014-02-27 01:09:51 +0000

    Added command line argument parsing code.

---
+++ CSSLimiter/Arguments.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Martin
+ * Date: 27/02/2014
+ * Time: 00:37
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using CommandLine;
+namespace CSSLimiter
+{
+	/// <summary>
+	/// Description of Arguments.
+	/// </summary>
+	public class Arguments
+	{
+	
+		
+		#region Constructor
+		public Arguments()
+		{
+			ContainingClass = string.Empty;
+			ContainingID = string.Empty;
+			ContainingType = string.Empty;
+		}
+		#endregion
+		
+		#region Properties
+		[CommandLine.Option('t',"type")]
+		public string ContainingType {get; set;}
+		
+		[CommandLine.Option('c',"class")]
+		public string ContainingClass {get; set;}
+		
+		[CommandLine.Option('i',"id")]
+		public string ContainingID {get;set;}
+		
+		#endregion
+		
+		
+	}
+}
--- CSSLimiter/Program.cs
+++ CSSLimiter/Program.cs
@@ -7,7 +7,7 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-
+using System.Threading.Tasks;
 namespace CSSLimiter
 {
 	class Program
@@ -16,10 +16,20 @@ namespace CSSLimiter
 		{
 			Console.WriteLine("Hello World!");
 			
-			// TODO: Implement Functionality Here
-			
+			Arguments commandLineArgs = GetArguments(args);
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
+		}
+		
+		private static Arguments GetArguments(string[] args)
+		{
+			Arguments result = new Arguments();
+			CommandLine.Parser parser = new CommandLine.Parser();
+			parser.ParseArguments(args, result);
+			
+			
+			return result;
+			
 		}
 	}
 }
